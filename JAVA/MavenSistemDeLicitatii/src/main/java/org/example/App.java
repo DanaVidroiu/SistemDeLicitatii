b5537148e05f70a50ba8600b47b9838857638c4a
DanaVidroiu <113728212+DanaVidroiu@users.noreply.github.com>
2024-05-30 12:08:57 +0300

    App.java

--- JAVA/MavenSistemDeLicitatii/src/main/java/org/example/App.java
+++ JAVA/MavenSistemDeLicitatii/src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3307/project";
+        String user = "root";
+        String password = "myPassword";
+
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexiunea la baza de date a fost realizata cu succes!");
+
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Eroare la conectare la baza de date: " + e.getMessage());
+        }
     }
 }
